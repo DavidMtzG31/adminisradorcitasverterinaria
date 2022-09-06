1fb08cc5caf961675f1cb56c4bdf9945b87a68ef
David Mtz G <dmartinezg1204@alumno.ipn.mx>
2022-09-05 22:51:23 -0500

    Eliminar citas

--- js/app.js
+++ js/app.js
@@ -20,8 +20,10 @@ class Citas {
 
     agregarCita(cita) {
         this.citas = [...this.citas, cita];
+    }
 
-        console.log(this.citas);
+    eliminarCita(id) {
+        this.citas = this.citas.filter(cita => cita.id !== id);
     }
 }
 
@@ -91,6 +93,14 @@ class UI{
                 <span class="font-weight-bolder"> Sintomas: </span> ${sintomas} 
             `;
 
+            // Botón para eliminar esta cita
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /> </svg>';
+
+            btnEliminar.onclick = () => eliminarCita(id)
+
+
             // Agregar los parrafos al div cita
             divCita.appendChild(mascotaParrafo);
             divCita.appendChild(propietarioParrafo);
@@ -98,6 +108,7 @@ class UI{
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
 
             // Agregar las citas al HTML
@@ -164,8 +175,8 @@ function nuevaCita(puta) {
     } if (!/^[a-zA-Z\ áéíóúÁÉÍÓÚñÑ\s]*$/.test(propietario)) {
         ui.imprimirAlerta('El nombre del propietario solo debe incluir letras', 'error');
         return;
-    } if ( telefono.length < 10 ) {
-        ui.imprimirAlerta('El teléfono debe ser a 10 dígitos', 'error');
+    } if ( telefono.length < 10 || telefono.length > 10 || !/^[0-9]*$/.test(telefono) ) {
+        ui.imprimirAlerta('El teléfono debe ser a 10 dígitos y solo debe contener números', 'error');
         return;
     }
 
@@ -197,4 +208,15 @@ function reiniciarObjeto() {
     citaObj.hora = '';
     citaObj.sintomas = '';
 
+}
+
+function eliminarCita(id) {
+    // Eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    // Muestra un mensaje
+    ui.imprimirAlerta('La cita se eliminó correctamente', 'success');
+
+    // Refrescar las citas
+    ui.imprimirCitas(administrarCitas);
 }
